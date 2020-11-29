c61f468e19d104c25b724aff994ebd5010023eb1
TCross <t05401btv@gmail.com>
2020-11-28 21:57:23 -0500

    Added packages for UI. Added shortend JSON files to test. Fixed Fragments and added ViewModels for multiple fragments

---
+++ app/src/main/java/com/example/vt251club/ui/map/MapFragment.java
@@ -0,0 +1,8 @@
+package com.example.vt251club.ui.map;
+
+import androidx.fragment.app.Fragment;
+
+public class MapFragment extends Fragment {
+
+
+}
---
+++ app/src/main/java/com/example/vt251club/ui/progress/ProgressFragment.java
@@ -0,0 +1,7 @@
+package com.example.vt251club.ui.progress;
+
+import androidx.fragment.app.Fragment;
+
+public class ProgressFragment extends Fragment {
+
+}
