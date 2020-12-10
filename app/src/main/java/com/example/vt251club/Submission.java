6e624b3765b8a1b51a2c8fcac433201a78c430a9
EthanBennett52 <72234446+EthanBennett52@users.noreply.github.com>
2020-12-09 19:30:40 -0500

    Added Functionality for submission

--- app/src/main/java/com/example/vt251club/Submission.java
+++ app/src/main/java/com/example/vt251club/Submission.java
@@ -2,31 +2,80 @@ package com.example.vt251club;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Arrays;
+
 
 public class Submission extends AppCompatActivity {
+    AutoCompleteTextView searchTextField;
+    EditText submissionTextField;
+    CheckBox shareCheckBox;
+    Button addImageButton;
+
+    private static final int PICK_IMAGE = 10;
+    Uri imageUri;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
 
-        Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.town_name_list, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        addImageButton = findViewById(R.id.addImageButton);
+        searchTextField = findViewById(R.id.submissionSearch);
+        submissionTextField = findViewById(R.id.submissionText);
+        shareCheckBox = findViewById(R.id.checkBox);
+
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.town_name_list, android.R.layout.select_dialog_singlechoice);
+        AutoCompleteTextView search = findViewById(R.id.submissionSearch);
+        search.setThreshold(3);
+        search.setAdapter(adapter);
 
     }
 
+    public void submit(View view){
+        String submissionText = String.valueOf((submissionTextField.getText()));
+        String searchText = String.valueOf(searchTextField.getText());
+        if(!Arrays.asList(getResources().getStringArray(R.array.town_name_list)).contains(searchText)){
+            Snackbar.make(view, "That isn't a town", Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+        }else if(submissionText.matches("")){
+            Snackbar.make(view, "You can submit and empty description", Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+        }
+        if(Arrays.asList(getResources().getStringArray(R.array.town_name_list)).contains(searchText) && !submissionText.matches("")){
+            //Put code to Enter submission in db here
+            
+        }
+    }
+
+    public void addImage(View view){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK){
+            imageUri = intent.getData();
+        }
+        addImageButton.setText("Change Image");
+    }
 
 
 }
